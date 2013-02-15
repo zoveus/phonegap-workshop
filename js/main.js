5a21839a022404fc0c4423f442db471c1b921222
Janis Kosarew <zoveus@gmail.com>
2013-02-15 18:23:33 +0100

    Part 5 of Tutorial

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+	},
+	renderHomeView: function() {
+		$('body').html(this.homeTpl());
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+		var self = this;
+		
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+		
+		this.store = new MemoryStore(function() {
+			self.renderHomeView();
+		});
+	}
 
 };
 
